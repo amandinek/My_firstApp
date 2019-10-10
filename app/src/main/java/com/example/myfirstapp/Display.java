c51ba9cbb23b4787a08fe963ce6bc100df67ab04
amandinek <karambiziga@gmail.com>
2019-10-10 15:07:56 +0200

    get user input

--- app/src/main/java/com/example/myfirstapp/Display.java
+++ app/src/main/java/com/example/myfirstapp/Display.java
@@ -4,17 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Display extends AppCompatActivity {
-
+    private TextView mMyTitle;
+    private TextView mMyTime;
+    private TextView mMyTasks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+        mMyTitle =(TextView)findViewById(R.id.myTasks);
+        mMyTime=(TextView)findViewById(R.id.myTime);
+        mMyTasks=(TextView)findViewById(R.id.myTasks);
         Intent intent = getIntent();
         String title = intent.getStringExtra("title");
         String timeDate = intent.getStringExtra("timeDate");
         String task = intent.getStringExtra("task");
+
+
+
+        mMyTitle.setText("My tasks" +"\n"+ title);
+        mMyTime.setText( timeDate);
+        mMyTasks.setText( task);
+
 
     }
 }
