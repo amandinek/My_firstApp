71fadea3582f5b4d80617e2f8fe34ad26ed9264b
amandinek <karambiziga@gmail.com>
2019-10-10 13:16:03 +0200

    add an intent in yourlist activity

--- app/src/main/java/com/example/myfirstapp/Display.java
+++ app/src/main/java/com/example/myfirstapp/Display.java
@@ -2,6 +2,7 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Display extends AppCompatActivity {
@@ -10,5 +11,10 @@ public class Display extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+        String timeDate = intent.getStringExtra("timeDate");
+        String task = intent.getStringExtra("task");
+
     }
 }
--- app/src/main/java/com/example/myfirstapp/YourList.java
+++ app/src/main/java/com/example/myfirstapp/YourList.java
@@ -2,13 +2,46 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class YourList extends AppCompatActivity {
+
+    private EditText mTitle;
+    private EditText mDate;
+    private EditText mTask;
+    private Button mAcivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_your_list);
+        mTitle =(EditText) findViewById(R.id.title);
+        mDate =(EditText) findViewById(R.id.date);
+        mTask =(EditText) findViewById(R.id.task);
+        mAcivity=(Button) findViewById(R.id.viewact);
+
+        mAcivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = mTitle.getText().toString();
+                String timeDate = mDate.getText().toString();
+                String task = mTask.getText().toString();
+
+                Intent intent = new Intent(YourList.this, Display.class);
+                intent.putExtra("title", title);
+                intent.putExtra("timeDate", timeDate);
+                intent.putExtra("task", task);
+
+                startActivity(intent);
+
+            }
+        });
+
+
+
     }
 }
