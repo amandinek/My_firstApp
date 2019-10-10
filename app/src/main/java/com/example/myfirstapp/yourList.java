ef278a5f349a45ab657a4bb6d3c0649d28226462
amandinek <karambiziga@gmail.com>
2019-10-10 10:56:35 +0200

    first commit

---
+++ app/src/main/java/com/example/myfirstapp/yourList.java
@@ -0,0 +1,14 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class yourList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_your_list);
+    }
+}
