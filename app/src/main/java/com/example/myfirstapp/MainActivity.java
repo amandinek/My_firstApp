134d86500e86dd61e9b4b09d30063bda76efd704
amandinek <karambiziga@gmail.com>
2019-10-10 12:09:53 +0200

    add an intent in main activity

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mSendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSendButton=(Button) findViewById(R.id.sendButton);
+
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, YourList.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myfirstapp/YourList.java
+++ app/src/main/java/com/example/myfirstapp/YourList.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class yourList extends AppCompatActivity {
+public class YourList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
